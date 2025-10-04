d0a9ddd3c227e34e53c839a5eb99b77c307c18d1
Kelvin <kelvinjuma99@gmail.com>
2025-10-04 19:52:05 +0300

    generics

--- TypeScript/TypeScript_Fundamentals/src/index.ts
+++ TypeScript/TypeScript_Fundamentals/src/index.ts
@@ -23,9 +23,14 @@ const menu: Pizza[] = [
 
 const orderHistory: Order[] = [];
 
-function addNewPizza(pizzaObj: Pizza): void {
-  pizzaObj.id = nextPizzaId++;
-  menu.push(pizzaObj);
+function addNewPizza(pizzaObj: Omit<Pizza, "id">): Pizza {
+  const newPizza: Pizza = {
+    id: nextPizzaId++,
+    ...pizzaObj,
+  };
+
+  menu.push(newPizza);
+  return newPizza;
 }
 
 function placeOrder(pizzaName: string): Order | undefined {
